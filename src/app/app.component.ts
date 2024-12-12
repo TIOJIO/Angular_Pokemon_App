b8d1fccecd51b774c2aa9f379cc54dc09e7dfa5a
tiojio romain junior <tiojioromain02@gmail.com>
2024-12-12 23:06:48 +0100

    init projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,34 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component,OnInit } from '@angular/core';
+ import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
+  standalone: true, 
+  imports: [CommonModule],
+  templateUrl:'./app.component.html',
   styles: [],
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit{
+  pokemonsList:Pokemon[] =POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit() {
+   // console.table(this.pokemonsList[0]);
+  }
+
+  selectPokement(pokemonId:string){
+
+    const pokemon : Pokemon|undefined = this.pokemonsList.find(pokemon => pokemon.id== +pokemonId)
+    if(pokemon){
+      console.log(`vous avez demander ${pokemon.name}`)
+      this.pokemonSelected=pokemon;
+    }else{
+       console.log('vous avez demander un pokemon qui n existe pas')
+       this.pokemonSelected=pokemon;
+      }
+
+  }
+  
 }
